66a30d64f365e009f036ab724037fb5c9ee0e3dd
Konrad <konslo96@gmail.com>
2023-05-24 21:10:45 +0200

    feat: Add tabs toggler

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,12 +1,28 @@
+'use client';
+
 import React from 'react';
 import classes from './Projects.module.scss';
+import { useState } from 'react';
 
 const Projects = () => {
+  const [activeTab, setActiveTab] = useState('projects');
+
+  const changeTabHandler = (tabName: string) => {
+    setActiveTab(tabName);
+  };
+
   return (
     <section className={classes.root}>
       <div className={classes.tabs}>
-        <button className={classes.active}>Projects</button>
-        <button>Contributions</button>
+        <button className={activeTab === 'projects' ? classes.active : ''} onClick={() => changeTabHandler('projects')}>
+          Projects
+        </button>
+        <button
+          className={activeTab === 'contributions' ? classes.active : ''}
+          onClick={() => changeTabHandler('contributions')}
+        >
+          Contributions
+        </button>
       </div>
       <div className={classes.projects}>
         <div className={classes.singleProject}></div>
