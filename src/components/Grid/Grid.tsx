862b33a566904d7d874da5fc0508c65a7d2b99c0
Konrad <konslo96@gmail.com>
2023-05-24 13:52:11 +0200

    feat: Add GithubTile component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './Grid.module.scss';
 import SpotifyTile from './Tiles/SpotifyTile/SpotifyTile';
+import GithubTile from './Tiles/GithubTile/GithubTile';
 
 const Grid = () => {
   return (
     <section className={classes.root}>
       <SpotifyTile />
+      <GithubTile />
     </section>
   );
 };
