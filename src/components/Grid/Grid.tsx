35eb5772ce0a6ddda4e7468984aa8cd5d12285bc
Konrad <konslo96@gmail.com>
2023-05-24 14:12:55 +0200

    feat: Add AboutTile component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import classes from './Grid.module.scss';
 import SpotifyTile from './Tiles/SpotifyTile/SpotifyTile';
 import GithubTile from './Tiles/GithubTile/GithubTile';
+import AboutTile from './Tiles/AboutTile/AboutTile';
 
 const Grid = () => {
   return (
     <section className={classes.root}>
       <SpotifyTile />
       <GithubTile />
+      <AboutTile />
     </section>
   );
 };
