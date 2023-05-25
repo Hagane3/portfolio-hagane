01adee2c0a5ebb2f3a78b72ae98fc0fd21005f4e
Konrad <konslo96@gmail.com>
2023-05-25 17:52:58 +0200

    refactor: Create new Tile component and apply to existing items

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -7,14 +7,28 @@ import LinkedinTile from './Tiles/LinkedinTile/LinkedinTile';
 import DiscordTile from './Tiles/DiscordTile/DiscordTile';
 import CommunityTile from './Tiles/CommunityTile/CommunityTile';
 
+import GithubIcon from '@/assets/icons/github.svg';
+import DiscordIcon from '@/assets/icons/discord.svg';
+import LinkedinIcon from '@/assets/icons/linkedin.svg';
+
+import Tile from '../UI/Tile/Tile';
+import Image from 'next/image';
+
 const Grid = () => {
   return (
     <section className={classes.root}>
       <SpotifyTile />
-      <GithubTile />
+      <Tile href="https://github.com/Hagane3" color="#777777">
+        <Image src={GithubIcon} alt="github icon" />
+      </Tile>
+      <Tile href="https://www.linkedin.com/in/konradslomiany/" color="#0e76a8">
+        <Image src={LinkedinIcon} alt="linkedin icon" />
+      </Tile>
+
       <AboutTile />
-      <LinkedinTile />
-      <DiscordTile />
+      <Tile href="" color="#7289da">
+        <Image src={DiscordIcon} alt="discord icon" />
+      </Tile>
       <CommunityTile />
     </section>
   );
