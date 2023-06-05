86ba08726d0c557ab60e65f0213b82f8bd43bf12
Konrad <konslo96@gmail.com>
2023-06-05 08:15:33 +0200

    fix: Fixed redirect bug when site deployed

--- src/utils/getAllProjects.ts
+++ src/utils/getAllProjects.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import matter from 'gray-matter';
+import path from 'path';
 
 export function getAllProjects() {
-  const allRawProjects = fs.readdirSync('src/content/projects');
+  const PROJECTS_DIR = path.join(process.cwd(), 'src/content/projects');
+  const allRawProjects = fs.readdirSync(PROJECTS_DIR);
   const allConvertedProjects = allRawProjects.map((project) => {
     const fileContents = fs.readFileSync(`src/content/projects/${project}`, 'utf8');
     const projectName = project.replace('.md', '');
