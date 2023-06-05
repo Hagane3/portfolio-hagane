6f5acfa59594ca7e696e131f67a6cececc944ea6
Konrad <konslo96@gmail.com>
2023-06-05 14:49:48 +0200

    feat: Add go back (previous page) func

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,16 +4,28 @@ import React from 'react';
 import Image from 'next/image';
 import classes from './Navbar.module.scss';
 import ThemeIcon from '@/assets/icons/theme-change.svg';
+import BackIcon from '@/assets/icons/left-arrow.svg';
 import { useTheme } from '@/context/ThemeContext';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const Navbar = () => {
   const { setThemeHandler } = useTheme();
+  const pathname = usePathname();
+  console.log(pathname);
 
   return (
     <nav className={classes.root}>
-      <button onClick={setThemeHandler}>
-        <Image src={ThemeIcon} alt="theme change icon" />
-      </button>
+      <div className={classes.container}>
+        <button onClick={setThemeHandler}>
+          <Image src={ThemeIcon} alt="theme change icon" />
+        </button>
+        {pathname !== '/' && (
+          <Link className={classes.backIcon} href="#">
+            <Image src={BackIcon} alt="back icon" />
+          </Link>
+        )}
+      </div>
     </nav>
   );
 };
