c5b940655ca8c8bc9948ae031ec234bee07b963c
Konrad <konslo96@gmail.com>
2023-06-19 19:28:48 +0200

    feat: Add chat bubble to Avatar Tile

--- src/components/Grid/Tiles/AvatarTile/AvatarTile.tsx
+++ src/components/Grid/Tiles/AvatarTile/AvatarTile.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 import React from 'react';
+import { useState } from 'react';
 
 import classes from './AvatarTile.module.scss';
 import Image from 'next/image';
 import avatarOk from '@/assets/images/avatar-ok.webp';
 
 const SpotifyTile = () => {
+  const [isBubbleVisible, setIsBubbleVisible] = useState(true);
+
+  const toggleBubble = () => {
+    setIsBubbleVisible((prevState) => !prevState);
+  };
+
   return (
     <div className={classes.root}>
-      <Image src={avatarOk} alt="avatar with macbook" />
+      <Image src={avatarOk} alt="avatar with macbook" onClick={toggleBubble} />
+      <p className={classes.chatBubble}>Let's create something!</p>
     </div>
   );
 };
